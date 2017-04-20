451892c35773e7d083bca4f19972f4c5852aff6d
Ginny Fahs <ginnyfahs@gmail.com>
2017-04-20 06:40:21 -0700

    Initial commit

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+c_name = gets.chomp
+
+puts "How old are you?"
+c_age = gets.chomp
+
+puts "What year were you born?"
+birth_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Do you want us to order you some? (y/n)"
+garlic_bread = gets.chomp 
+
+puts "Would you like to enroll in the company’s health insurance?"
+insurance = gets.chomp
