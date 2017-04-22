81d7c63b8099a5a7deed73a545402460c6043df0
Ginny Fahs <ginnyfahs@gmail.com>
2017-04-21 20:44:39 -0700

    Proper logic and loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,9 @@
+puts "How many employees do you wish to process?"
+e_to_process = gets.chomp.to_i 
+
+
+until e_to_process == 0
+
 puts "What is your name?"
 c_name = gets.chomp
 
@@ -6,10 +12,12 @@ c_age = gets.chomp.to_i
 
 puts "What year were you born?"
 birth_year = gets.chomp.to_i
-if birth_year + c_age == 2016 || 2017 
+if birth_year + c_age == 2016 || birth_year + c_age == 2017 
 	age = true
+	puts "age is true"
 else
 	age = false
+	puts "age is false"
 end 
 
 puts "Our company cafeteria serves garlic bread. Do you want us to order you some? (y/n)"
@@ -28,27 +36,18 @@ else
 	insurance = false
 end 
 
-#if age is correct && (garlic bread = yes || insurance = yes) 
-#puts "Probably not vampire"
-if age == true && (garlic_bread == true || insurance == true)
-	puts "Probably not a vampire."
-
-#if age is wrong && (hates garlic || insurance = no) 
-# puts "Probably a vampire"
-elsif age == false && (garlic_bread == false || insurance == false)
-	puts "Probably a vampire"
-
-# if age is wrong && hates garlic && no insurance
-# puts "Almost certainly a vampire"
-elsif age == false && garlic_bread == false && insurance == false
-	puts "Almost certainly a vampire"
-
-# If c_name == "Drake Cula" || c_name == "Tu Fang"
-# puts "Definitely a vampire."
-elsif c_name == "Drake Cula" || c_name == "drake cula" || c_name == "Tu Fang" || c_name == "tu fang"
+case 
+when c_name == "Drake Cula" || c_name == "drake cula" || c_name == "Tu Fang" || c_name == "tu fang"
 	puts "Definitely a vampire!!!!!!!!!"
-
-# else puts "Results inconclusive."
+when age == true && (garlic_bread == true || insurance == true)
+	puts "Probably not a vampire."
+when age == false && garlic_bread == false && insurance == false
+	puts "Almost certainly a vampire"
+when age == false && (garlic_bread == false || insurance == false)
+	puts "Probably a vampire"
 else
 	puts "Results inconclusive."
 end 
+
+e_to_process = e_to_process - 1
+end
