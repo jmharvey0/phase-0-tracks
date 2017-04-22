2f230ba8940c402af00c9d517e026f70ad03d746
Josh <jmharvey0@gmail.com>
2017-04-22 14:42:47 -0500

    change to while loop from for loop

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -5,15 +5,17 @@
 #output -> "bcd"
 
 def encrypt(pass)
-
 	index = 0
-	for index in 0..pass.length
+	while index < pass.length
 		#if pass[index] != "z" or "Z" This is for in case of End case problem ("z".next = "aa")
 			pass[index] = pass[index].next
+			index += 1
 	end
+	puts pass
 end
 #DECRYPT
 #input -> "bcd"
 #Return string is 1 character less than the input For each character
 # *Exception, Spaces remain unchanged*
 #output -> "abc"
+
