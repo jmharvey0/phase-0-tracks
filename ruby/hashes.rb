d12832ea86e53ee1ad26386db2eeb20e9a7a0497
Ginny Fahs <ginnyfahs@gmail.com>
2017-04-25 23:08:08 -0700

    boolean correct

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -14,21 +14,20 @@ puts "Enter the client's desired decor theme."
 designer_response[:decor_theme] = gets.chomp.to_s
 
 puts "Is this client picky AF? (y/n)"
-designer_response[:picky] = gets.chomp
-	if :picky == "yes" || :picky == "y"
-		true
+designer_response[:picky] = gets.chomp.to_s 
+	if designer_response[:picky] == "yes" || designer_response[:picky] == "y"
+		designer_response[:picky] = true
 	else
-		false
+		designer_response[:picky] = false
 	end 
 
 p designer_response
 
-puts "Do you need to update any information? (y/n)"
-	update = gets.chomp.to_s
-	if update == "yes" || update == "y"
-		puts "Type the name of the key you wish to update."
+puts "If you need to make an update, please enter the name of the key you wish to update. If you don't require any updates, say 'none'."
 		key_change = gets.chomp.to_s
-			if key_change == "name"
+			if key_change == "none"
+				exit
+			elsif key_change == "name"
 				puts "Enter your correction for name."
 					designer_response[:name] = gets.chomp.to_s
 			elsif key_change == "age"
@@ -43,21 +42,10 @@ puts "Do you need to update any information? (y/n)"
 			elsif key_change == "picky"
 				puts "Enter your correction: Is the client picky AF?"
 				designer_response[:picky] = gets.chomp
-					if :picky == "yes" || :picky == "y"
-						true
+					if designer_response[:picky] == "yes" || designer_response[:picky] == "y"
+						designer_response[:picky] = true
 					else
-						false
-					end 
-			else
-				puts "Sorry--I don't understand. Please type the exact name of the key you wish to update."
-				#loop to above the key_change // gets.chomp line				
+						designer_response[:picky] = false
+					end 		
 			end
 		p designer_response
-	else
-		exit
-	end 
-
-
-# let user update a key (once is fine) (Hint: Strings have methods that will turn them into symbols) 
-# print final version of hash
-# exit program
