3ce6854e49394afd47a5e72469a3c26ddfc25f7f
Josh <jmharvey0@gmail.com>
2017-04-27 11:51:04 -0500

    add repetition for survey fo Vampire-detection

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -15,22 +15,28 @@ def detection(vampName, age, birthYear, garlicBread, insurance)
 	return vampireDetection
 end
 
-#What's your name?
-puts "What's your name?"
-vampName = gets.chomp
-#How old are you? & What year were you born?
-puts "How old are you?"
-age = gets.chomp
+puts "How many Employees will be processed in today's survey?"
+vampNum = gets.chomp.to_i
 
-puts "What year were you born?"
-birthYear = gets.chomp
-#Garlic bread
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
-garlicBread = gets.chomp
-#Would you like to enroll in Health insurance?
-puts "Would you like to enroll in the company’s health insurance? (Y/N)"
-insurance = gets.chomp
+for i in 1..vampNum
+	puts "Processing Employee #{i} out of #{vampNum}"
+	#What's your name?
+	puts "What's your name?"
+	vampName = gets.chomp
+	#How old are you? & What year were you born?
+	puts "How old are you?"
+	age = gets.chomp
 
-vampireDetection = detection(vampName, age, birthYear, garlicBread, insurance)
+	puts "What year were you born?"
+	birthYear = gets.chomp
+	#Garlic bread
+	puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
+	garlicBread = gets.chomp
+	#Would you like to enroll in Health insurance?
+	puts "Would you like to enroll in the company’s health insurance? (Y/N)"
+	insurance = gets.chomp
 
-puts "Results: #{vampireDetection}" 
+	vampireDetection = detection(vampName, age, birthYear, garlicBread, insurance)
+
+	puts "Results: #{vampireDetection}" 
+end
