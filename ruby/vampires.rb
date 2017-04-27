12d452e85a206b91c857662fab662291878a69ab
Josh <jmharvey0@gmail.com>
2017-04-27 11:22:49 -0500

    add data collection for Vampire-detection

---
+++ ruby/vampires.rb
@@ -0,0 +1,16 @@
+#Vampire-Detection
+#What's your name?
+puts "What's your name?"
+vampName = gets.chomp
+#How old are you? & What year were you born?
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+birthYear = gets.chomp
+#Garlic bread
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
+garlicBread = gets.chomp
+#Would you like to enroll in Health insurance?
+puts "Would you like to enroll in the company’s health insurance? (Y/N)"
+insurance = gets.chomp
