b22ee7d0ca20e0e8336156724fbd41af13b1dc03
Josh <jmharvey0@gmail.com>
2017-04-27 12:19:58 -0500

    Fix garlicBread and insurance to Boolean, fix logic after testing for Vampire-detection

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,16 +1,20 @@
 #Vampire-Detection
 
 def detection(vampName, age, birthYear, garlicBread, insurance)
+
+	if age.to_i == (2017 - birthYear.to_i) && (garlicBread || insurance)
+			vampireDetection = "Probably not a vampire"
+	elsif age.to_i != (2017 - birthYear.to_i)
+		if !garlicBread || !insurance
+			vampireDetection = "Proabably a vampire"
+		elsif !garlicBread && !insurance
+			vampireDetection = "Almost certainly a vampire"
+		else 		
+			vampireDetection = "Inconclusive"
+		end
+	end
 	if vampName == "Drake Cula" || "Tu Fang"
 		vampireDetection = "Definitely a vampire"
-	elsif age == (2017 - birthYear) && (garlicBread || insurance)
-		vampireDetection = "Probably not a vampire"
-	elsif age != (2017 - birthYear) && !(garlicBread || insurance)
-		vampireDetection = "Proabably a vampire"
-	elsif age != (2017 - birthYear) && !garlicBread && !insurance
-		vampireDetection = "Almost certainly a vampire"
-	else 		
-		vampireDetection = "Inconclusive"
 	end
 	return vampireDetection
 end
@@ -26,17 +30,22 @@ for i in 1..vampNum
 	#How old are you? & What year were you born?
 	puts "How old are you?"
 	age = gets.chomp
-
 	puts "What year were you born?"
 	birthYear = gets.chomp
 	#Garlic bread
 	puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
-	garlicBread = gets.chomp
+	garlicBread = gets.chomp.to_s
 	#Would you like to enroll in Health insurance?
 	puts "Would you like to enroll in the company’s health insurance? (Y/N)"
-	insurance = gets.chomp
+	insurance = gets.chomp.to_s
 
 	vampireDetection = detection(vampName, age, birthYear, garlicBread, insurance)
 
 	puts "Results: #{vampireDetection}" 
-end
+end	
+elsif  && ()
+	
+elsif  && !(garlicBread || insurance)
+	
+elsif age.to_i != (2017 - birthYear.to_i) && !garlicBread && !insurance
+	
