6e6e690aaf89aa0cf6080059a3e48fefa1a0f05d
Ginny Fahs <ginnyfahs@gmail.com>
2017-04-29 12:11:16 -0700

    confusing ignore

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,8 +1,4 @@
 #RELEASE 0
-#takes an array of integers
-# takes an integer to search for
-#returns the index of the item, or nil if no item
-
 starting_array = [1, 3, 5, 7, 9]
 
 def search_array(starting_array, value) 
