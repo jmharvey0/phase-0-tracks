217eeb0ef14c5826b5447025ab194478d63ea196
Josh <jmharvey0@gmail.com>
2017-05-02 17:32:14 -0500

    add prompts for data collection

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,14 +1,25 @@
 #Interior Designers database
 #Collecting data for interior design clinets
 #Name
-# => get a string w/ a space
+# => string w/ a space
 #Age
-# => get an interger
+# => interger
 #Number of Children
 # => Interger of Children
 #Kid friendly?
 # => boolean
 #Decor Theme
 # => string
-#Sandproof decor?
-# => boolean
+
+
+design_clinets = {}
+
+puts "What's the clinet's Name?"
+
+puts "How old is the clinet?"
+
+puts "How many children do they have?"
+
+puts "Does their home have to be Kid Friendly?"
+
+puts "What theme does the Clinet want for their decor?"
