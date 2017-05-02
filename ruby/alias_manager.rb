8b2936fce6c94a3c445f226542cf7a2f3955f817
Ginny Fahs <ginnyfahs@gmail.com>
2017-05-01 21:38:16 -0700

    Initial commit

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,12 @@
+#take real name
+real_name = "Felicia Torres"
+
+def crazy_alias(real_name)
+real_name.split(' ')
+end
+
+puts crazy_alias
+#swap first and last name
+#create new empty arrays for swapped_first and swapped_last
+# fill in old data from existing list and use new arrays moving forward
+#create fake name
