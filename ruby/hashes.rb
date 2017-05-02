a130f7ac77de511ac1853ae24376fd78cf1324f7
Josh <jmharvey0@gmail.com>
2017-05-02 16:18:37 -0500

    pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,14 @@
+#Interior Designers database
+#Collecting data for interior design clinets
+#Name
+# => get a string w/ a space
+#Age
+# => get an interger
+#Number of Children
+# => Interger of Children
+#Kid friendly?
+# => boolean
+#Decor Theme
+# => string
+#Sandproof decor?
+# => boolean
