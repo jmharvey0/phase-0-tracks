83f753cfe0ebaa0673602eb09cb70f1f2169fe65
Josh <jmharvey0@gmail.com>
2017-05-02 18:21:50 -0500

    add fixes after testing

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,3 +1,13 @@
+#Boolean coversion thanks to ChuckJHardySnippets
+#https://gist.github.com/ChuckJHardySnippets/2000623
+class String
+  def to_bool
+    return true   if self == true   || self =~ (/(true|t|yes|y|1)$/i)
+    return false  if self == false  || self.blank? || self =~ (/(false|f|no|n|0)$/i)
+    raise ArgumentError.new("invalid value for Boolean: \"#{self}\"")
+  end
+end
+
 #Interior Designers database
 #Collecting data for interior design clinets
 #Name
@@ -24,7 +34,7 @@ puts "How many children do they have?"
 design_clinets[:children_num] = gets.chomp.to_i
 
 puts "Does their home have to be Kid Friendly? (Y/N)"
-design_clinets[:kid_friendly] = gets.chomp.to_s
+design_clinets[:kid_friendly] = gets.chomp.to_bool
 
 puts "What theme does the Clinet want for their decor?"
 design_clinets[:decor_theme] = gets.chomp
@@ -32,26 +42,24 @@ design_clinets[:decor_theme] = gets.chomp
 p design_clinets
 
 puts "Would you like to update any data? If so please enter the key for it."
-key = gets.chomp.to_sym
+key = gets.chomp
 
 case key
-when key == :kid_friendly
-	puts "Does their home have to be Kid Friendly? (Y/N)"
-	design_clinets[:kid_friendly] = gets.chomp.to_s
-when key == :age
-	puts "How old is the clinet?"
-	design_clinets[:age] = gets.chomp.to_i
-when key == :name
+when key == name
 	puts "What's the clinet's Name?"
 	design_clinets[:name] = gets.chomp
-when key == :children_num
+when key == age
+	puts "How old is the clinet?"
+	design_clinets[:age] = gets.chomp.to_i
+when key == children_num
 	puts "How many children do they have?"
 	design_clinets[:children_num] = gets.chomp.to_i
-when key == :decor_theme
+when key == kid_friendly
+	puts "Does their home have to be Kid Friendly? (Y/N)"
+	design_clinets[:kid_friendly] = gets.chomp.to_bool
+when key == decor_theme
 	puts "What theme does the Clinet want for their decor?"
 	design_clinets[:decor_theme] = gets.chomp
-else
-	next
 end
 
 p design_clinets
