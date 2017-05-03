e8c503859489e0bbd83ca6f1fb4ee53d12a1b967
Josh <jmharvey0@gmail.com>
2017-05-02 19:22:00 -0500

    add fixes after testing

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,34 +1,38 @@
-us-government = {
-	legestrative: {
-		house: {
-			house_speaker: "Paul Ryan"
-			representatives: 453
-			duties: {
-				"Introduce Bills", "Approve Bills"
-			}
+us_government = {
+	legestrative:{
+		house:{
+			house_speaker: 'Paul Ryan',
+			representatives: 453,
+			duties: [
+				'Introduce Bills', 
+				'Approve Bills'
+				]
 		}
-		sentate: {
-			senate_president: "Mike Pence"
-			sentators: 100
+		senate:{
+			senate_president: 'Mike Pence',
+			sentators: 100,
 			duties: {
-				"Introduce Bills", "Approve Bills", "Approve Presidental Appointment"
-			}
+				bill: [
+					'Introduce Bills', 'Approve Bills'
+					],
+				appointment: 'Approve Presidental Appointment'
+				}
 		}
-	}
 	judiciary:{
-		chief_justice: "John Roberts"
-		justices: 9
-		term_limit: "Life Tenure"
-		duties: {
-			"Interpret the Constitution", "Adjudicates cases and controversies"
+		chief_justice: 'John Roberts',
+		justices: 9,
+		term_limit: 'Life Tenure',
+		duties: [
+			'Interpret the Constitution', 'Adjudicates cases and controversies'
+			]
+		}
+	executive:{
+		president: 'Donald J. Trump',
+		vice_president: 'Mike Pence'
 		}
 	}
-	executive:{
-		president: "Donald J. Trump"
-		vice_president: "Mike Pence"
-	}
-}
 
 p executive[:president]
 p judiciary[:duties][1]
 p legestrative[:house_speaker]
+p legestrative[:senate][:duties][:bill][0]
