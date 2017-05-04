a6c23eb971544ff65525901c3f513290d0f0fec1
Ginny Fahs <ginnyfahs@gmail.com>
2017-05-04 07:01:41 -0700

    Thought about puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,13 @@
+# SPECIES ---------
+# dog
+
+# CHARACTERISTICS -----
+# breed: basset lab
+# fur color: black
+# height: 18 in
+# name: hershey
+
+# BEHAVIOR------
+# Slow lick
+# Bashful
+# Eat poop
