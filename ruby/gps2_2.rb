6145243eeeb26dc0f95d34f8a770a7d7cba553fb
Josh <jmharvey0@gmail.com>
2017-05-03 20:46:47 -0500

    puesdocode for gps2.2

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,30 @@
+
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # Split given list (string) into a temp container [array]
+  # set default quantity to 1
+  # print the list to the console (Use print method that we'll create)
+# output: hash of items w/ quantities attached
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps:
+	#take item name (string) and put into the list {hash}
+	#update quantity if other than 1
+# output: updated list {hash}
+
+# Method to remove an item from the list
+# input:
+# steps:
+# output:
+
+# Method to update the quantity of an item
+# input:
+# steps:
+# output:
+
+# Method to print a list and make it look pretty
+# input:
+# steps:
+# output:
