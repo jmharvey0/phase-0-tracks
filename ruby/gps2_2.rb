211de94592f7b9cfb554eb8a8f60cf659ead4d68
Josh <jmharvey0@gmail.com>
2017-05-03 21:19:23 -0500

    refactoring for gps2.2

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -7,12 +7,28 @@
   # print the list to the console (Use print method that we'll create)
 # output: hash of items w/ quantities attached
 
+def create_list(items)
+	list = { }
+	items.split(' ').each do |item|
+		add_to_list(list, item)
+	end
+
+	p list
+	#print_list #If we want a pretty list printed out. Boss we need more infos
+end
+
 # Method to add an item to a list
 # input: list, item name, and optional quantity
 # steps:
 	#take item name (string) and put into the list {hash}
 	#update quantity if other than 1
 # output: updated list {hash}
+
+def add_to_list(list, item, quantity = 1)
+	list[item] = quantity
+
+	return list
+end
 
 # Method to remove an item from the list
 # input:
@@ -28,3 +44,17 @@
 # input:
 # steps:
 # output:
+
+#DIVER CODE-------------------------------------------------------------------------------------------------
+
+list = create_list("carrots apples cereal pizza")
+
+p list
+
+add_to_list(list, "banana")
+
+p list
+
+add_to_list(list, "squash", 5)
+
+p list
