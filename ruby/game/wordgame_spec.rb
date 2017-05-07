508f080867d20729e5912258093393695cb0e717
Ginny Fahs <ginnyfahs@gmail.com>
2017-05-07 11:19:35 -0700

    First test working

---
+++ ruby/game/wordgame_spec.rb
@@ -0,0 +1,35 @@
+require_relative 'wordgame'
+
+describe WordGame do 
+	let(:word) {WordGame.new(["hello"])}
+
+	it "stores the word given on initialization" do
+		expect(word.get_word).to eq ["hello"]
+	end
+
+	# it "breaks the word into a string of letters" do
+
+end
+
+#EXAMPLE CODE
+# describe TodoList do
+#   let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
+
+#   it "stores the list items given on initialization" do
+#     expect(list.get_items).to eq ["do the dishes", "mow the lawn"]
+#   end
+
+#   it "adds an item to the list" do
+#     list.add_item("mop")
+#     expect(list.get_items).to eq ["do the dishes", "mow the lawn", "mop"]
+#   end
+
+#   it "deletes an item" do
+#     list.delete_item("do the dishes")
+#     expect(list.get_items).to eq ["mow the lawn"]
+#   end
+
+#   it "retrieves an item by index" do
+#     expect(list.get_item(0)).to eq "do the dishes"
+#   end
+# end
