1d37e22c69b70f3595dba12c226e166eced98c5c
Ginny Fahs <ginnyfahs@gmail.com>
2017-05-06 17:14:38 -0700

    Complete release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,33 @@
-module Shout
-	 def self.yell_angrily(words)
-	 	words + "!!!!" + ":("
-	 end
+# module Shout
+# 	 def self.yell_angrily(words)
+# 	 	words + "!!!!" + ":("
+# 	 end
 
-	 def self.yelling_happily(words)
-	 	words + "!!!!" + ":)"
-	 end
+# 	 def self.yelling_happily(words)
+# 	 	words + "!!!!" + ":)"
+# 	 end
+# end
+
+# p Shout.yell_angrily("blergggg")
+# p Shout.yelling_happily("woohoo")
+
+module Shout
+	def yell(words)
+	puts "#{words}!!!!!!!"
+	end
 end
 
-p Shout.yell_angrily("blergggg")
-p Shout.yelling_happily("woohoo")
+class Kid
+	include Shout
+end
+
+class Teacher
+	include Shout
+end
+
+kid = Kid.new
+kid.yell("I want ice cream")
+
+teacher = Teacher.new 
+teacher.yell("Do your homework")
+
