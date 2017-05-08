940b84f71a07a13d2213dbc33e445ce487ddce33
Ginny Fahs <ginnyfahs@gmail.com>
2017-05-07 21:54:42 -0700

    first few methods

--- ruby/game/wordgame.rb
+++ ruby/game/wordgame.rb
@@ -5,9 +5,8 @@ class WordGame
 
 	def initialize(word)
 		@word = word 
-		# @guess_number = 0
-		# @is_correct = false
-		# @is_over = false
+		@length = word.length
+		@guesses = word.length
 	end
 
 	def get_word
@@ -20,42 +19,18 @@ class WordGame
 		#output is ["h", "e", "l", "l", "o"]
 	end
 
-	def space_writer
-		space_maker = “_ “ * @word.length
-		space_writer = []
-		space_writer << space_maker
-		return space_writer
+	def letter_slots
+		letter_slots = "_ " * @length
 	end
+
+	def letter_slot_breaker
+		letter_slot_breaker = letter_slots.split(" ")
+	end
+
 end
 
+#TESTING
+game = WordGame.new("hello")
+p game.letter_slot_breaker
 
-
-# def total_guesses
-# 	total_guesses = @word.length
-# end
-
-# def check_letter(index)
-# 	puts “Take a guess!”
-# 	user_guess = gets.chomp
-# 	stored_guesses << user_guess
-# 	@guess_count +=1
-# 		if word[index] == user_guess
-# 			@is_correct = true
-# 		else
-# 			false
-# 		end
-# 	end
-
-# =====
-# word is passed into method in driver code
-# guess should be initialized at 1
-
-# method should count the number of letters in the word using .length
-# use should see “_ “ * word.length
-# user should see “you have #{word.length} guesses to figure out the word! This is guess number #{guess_number}”
-
-
-# word <—string // array
-# guess_number (make sure to remove duplicates) <—integer
-# stored_guesses <—array
-# is_over <—boolean
+#DRIVER CODE
