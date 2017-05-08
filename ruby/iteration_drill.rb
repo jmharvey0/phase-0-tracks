696874f51a831ef7ec4f81ea06deb0056c7a2a73
Josh <jmharvey0@gmail.com>
2017-05-08 15:06:09 -0500

    init commit

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -6,6 +6,9 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # 1. Iterate through the zombie_apocalypse_supplies array,
 # printing each item in the array separated by an asterisk
 # ----
+zombie_apocalypse_supplies.each do |supplies|
+	p "#{zombie_apocalypse_supplies[supplies]}*"
+end
 
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. Do not use any special built-in methods.
