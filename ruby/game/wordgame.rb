e1fef38eaf54569f569a160a1d6c1114e4f3e7b2
Ginny Fahs <ginnyfahs@gmail.com>
2017-05-07 22:29:04 -0700

    switching gears

--- ruby/game/wordgame.rb
+++ ruby/game/wordgame.rb
@@ -1,12 +1,14 @@
 
 class WordGame
-	# attr_reader :guess_count
-	# attr_reader :is_over
+	attr_reader :guess_count
+	attr_reader :is_over
 
 	def initialize(word)
 		@word = word 
 		@length = word.length
-		@guesses = word.length
+		@total_guesses = word.length
+		@guess_count = 0
+		@is_over = false
 	end
 
 	def get_word
@@ -27,10 +29,24 @@ class WordGame
 		letter_slot_breaker = letter_slots.split(" ")
 	end
 
+	def letter_check(letter)
+		@guess_count +=1
+		word_breaker.each do |check|
+			if letter == check
+				#do something
+			elseif letter == THINGS THAT HAVE BEEN GUESSED BEFORE
+				#do something
+			else 
+				#do something
+			end
+	end
+
+
 end
 
-#TESTING
-game = WordGame.new("hello")
-p game.letter_slot_breaker
 
 #DRIVER CODE
+puts "Welcome to the Word Game!"
+game = WordGame.new("hello")
+
+puts "A secret word has been chosen ;)"
