1c9373f328a4211e3f4169ceec068576ffeedffe
Josh <jmharvey0@gmail.com>
2017-05-09 15:24:32 -0500

    Coach and father classes with driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,34 @@
+# module Shout
+# 	def self.yell_angrily(words)
+#     	words + "!!!" + " :("
+# 	end
+# 	def self.yelling_happily(words)
+# 		words + "!" + " :D"	
+# 	end
+# end
+
 module Shout
-	def self.yell_angrily(words)
+	def yell_angrily(words)
     	words + "!!!" + " :("
 	end
-	def self.yelling_happily(words)
+	def yelling_happily(words)
 		words + "!" + " :D"	
 	end
 end
 
+class Coach
+	include Shout
+end
+
+class Father
+	include Shout
+end
 #DRIVER CODE ----------------------------------------------------
 Shout.yell_angrily("Ruby")
 Shout.yelling_happily("Ruby")
+
+dave = Coach.new
+kevin = Father.new
+
+dave.yell_angrily("Come on")
+kevin.yell_angrily("Get off the Couch")
