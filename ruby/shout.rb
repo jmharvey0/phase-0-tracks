36d7d1fdd63a570d20654b562056c146253c7666
Josh <jmharvey0@gmail.com>
2017-05-09 15:18:57 -0500

    yelling emotionally with driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,5 +1,12 @@
 module Shout
-    def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+	def self.yell_angrily(words)
+    	words + "!!!" + " :("
+	end
+	def self.yelling_happily(words)
+		words + "!" + " :D"	
+	end
 end
+
+#DRIVER CODE ----------------------------------------------------
+Shout.yell_angrily("Ruby")
+Shout.yelling_happily("Ruby")
