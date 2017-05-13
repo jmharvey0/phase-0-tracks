75e507b851ee1f254f2c4d57be42c03f4569f0ad
Josh <jmharvey0@gmail.com>
2017-05-13 13:11:44 -0500

    add function reverse and driver code to test

---
+++ js/explore.js
@@ -0,0 +1,17 @@
+// function: reverse
+// input: string
+// steps:
+// 	walk through the string starting from the end
+// 	and add each character together into the reversed string
+// output: reversed string
+
+function reverse(str){
+	var revStr = '';
+	for (var i = str.length - 1; i >= 0; i--) {
+		revStr += str[i];
+	}
+	return revStr
+}
+
+//DRIVER CODE--------------------------------------------------------------------
+console.log(reverse("hello"));
