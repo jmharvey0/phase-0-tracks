f58a80acd3d787ba54265b7b10577e428d08a90b
Josh <jmharvey0@gmail.com>
2017-05-13 12:05:13 -0500

    add DRY driver code for printing all 50 states' data

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -86,7 +86,10 @@ california.virus_effects
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
 
-STATE_DATA["Puerto Rico"] = 42
+STATE_DATA.each do |state, population_data|
+  state = VirusPredictor.new(state, STATE_DATA[state][:population_density], STATE_DATA[state][:population])
+  state.virus_effects
+end
 
 #=======================================================================
 # Reflection Section
