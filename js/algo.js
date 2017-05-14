5ea3bbf0bebd150f7bdefe27a339b33d5f814fe1
Josh <jmharvey0@gmail.com>
2017-05-14 13:42:50 -0500

    add algos method with driver code tests.

--- js/algo.js
+++ js/algo.js
@@ -3,3 +3,19 @@
 // steps:
 // search through Array for longest element
 // output: return the longest element
+
+function algos(array){
+	var longest_length = 0, longest_index;
+	for (var i = 0; i < array.length; i++) {
+		if (array[i].length > longest_length){
+			longest_length = array[i].length;
+			longest_index = i;
+		}
+	}
+	return array[longest_index];
+}
+
+// DRIVER CODE----------------------------------------------------------------------
+var array = ["boom", "bam", "power", "powerful"]
+
+console.log("In this data set the greatest power word is " + algos(array) + ".")
