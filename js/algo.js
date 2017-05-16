341e4536dc900fa7e63da7f4ebbc7724e153783a
Josh <jmharvey0@gmail.com>
2017-05-16 10:03:21 -0500

    add key_compare function with driver code tests

--- js/algo.js
+++ js/algo.js
@@ -15,10 +15,19 @@ function algos(array){
 	return array[longest_index];
 }
 function key_compare(object1, object2) {
-	
+	var match = false;
+	Object.keys(object1).forEach(function(key){
+		if(object2.key == object1.key){
+			match = true;
+		}
+	});
+	return match;
 }
 
 // DRIVER CODE----------------------------------------------------------------------
 var array = ["boom", "bam", "power", "powerful"]
 
 console.log("In this data set the greatest power word is " + algos(array) + ".")
+
+console.log(key_compare({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+console.log(key_compare({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}));
