5fb29cb8c913ae84fdc1eea4e8870cb43c125de9
Josh <jmharvey0@gmail.com>
2017-05-16 10:31:35 -0500

    add random_array function with driver code tests

--- js/algo.js
+++ js/algo.js
@@ -23,7 +23,23 @@ function key_compare(object1, object2) {
 	});
 	return match;
 }
+function random_array(length) {
+	var random_array = [];
+	for (var i = 0 ; i < length; i++) {
+		random_array.push(randomString(Math.floor(Math.random() * (10 - 1)) + 1));
+		}
+	return random_array;
+}
 
+//Random word gen to simplify random_array code inspired by https://www.thepolyglotdeveloper.com/2015/03/create-a-random-nonce-string-using-javascript/
+function randomString(length) {
+	var text = "";
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	for (var i = 0; i < length; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
 // DRIVER CODE----------------------------------------------------------------------
 var array = ["boom", "bam", "power", "powerful"]
 
@@ -31,3 +47,7 @@ console.log("In this data set the greatest power word is " + find_longest(array)
 
 console.log(key_compare({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
 console.log(key_compare({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}));
+
+for (var i = 0; i < 10; i++) {
+	console.log(find_longest(random_array(i + 2)));
+}
