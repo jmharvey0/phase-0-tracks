54bca645e68c63a2d1224c4c14643008eb92e717
Ginny Fahs <ginnyfahs@gmail.com>
2017-05-21 12:42:57 -0700

    Should have done initial commit long ago

---
+++ databases/read_better/read_better.rb
@@ -0,0 +1,51 @@
+#OPERATION READ BETTER
+
+#require gems
+require 'sqlite3'
+
+#create SQLite3 database
+db = SQLite3::Database.new("reading_suggestions.db")
+db.results_as_hash = true
+
+#fancy string delimiters
+create_table = <<-SQL
+	CREATE TABLE IF NOT EXISTS reading_suggestions(
+	id INTEGER PRIMARY KEY,
+	title VARCHAR(255),
+	author VARCHAR(255),
+	type VARCHAR(255)
+	)
+SQL
+
+#create a table (if it's not there already)
+#add books
+db.execute(create_table)
+db.execute("INSERT INTO reading_suggestions (title, author, type) VALUES 
+	('The Vegetarian', 'Han Kang', 'fiction'),
+	('The Argonauts', 'Maggie Nelson', 'fiction'),
+	('White Teeth', 'Zadie Smith', 'fiction'),
+	('Citizen', 'Claudia Rankine', 'fiction'),
+	('All the Single Ladies', 'Anne Traister', 'non fiction'),
+	('Backlash', 'Susan Faludi', 'non fiction'),
+	('Behind the Beautiful Forevers', 'Katherine Boo', 'non fiction'),
+	('The Soul of an Octopus', 'Sy Montgomery', 'non fiction')
+	")
+
+#definte a method that will automatically generate a suggestion, based on whether user likes fiction or non fiction
+def create_suggestion(db)
+	puts "Do you like fiction or non fiction?"
+	answer = gets.chomp.to_s
+	db.execute("SELECT title, author FROM reading_suggestions WHERE type=answer ORDER BY RANDOM() LIMIT 1")
+	puts "You should read #{suggestion['title']} by #{suggestion['author']}!"
+end
+
+create_suggestion(db)
+
+# create_suggestion(db, "fiction")
+
+# #explore ORM by retrieving data
+# suggestions = db.execute("SELECT * FROM reading_suggestions")
+# suggestions.each do |suggestion|
+# 	puts "You should read #{suggestion['title']} by #{suggestion['author']}!"
+# end 
+
